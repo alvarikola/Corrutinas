34d9cbbe185b81a9a0779a9bb4f910a1bb5ce017
alvarikola <alvaroe484@gmail.com>
2024-11-08 12:14:58 +0000

    muestra los datos por pantalla

--- app/src/main/java/com/example/corrutinas/MainActivity.kt
+++ app/src/main/java/com/example/corrutinas/MainActivity.kt
@@ -1,43 +1,52 @@
 package com.example.corrutinas
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Text
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.lifecycleScope
 import com.example.corrutinas.ui.theme.CorrutinasTheme
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import retrofit2.Response
 
+
 class MainActivity : ComponentActivity() {
+    @SuppressLint("CoroutineCreationDuringComposition")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        var retrofit = RetrofitHelper.getInstance()
-        lifecycleScope.launch(Dispatchers.IO){
-            val resultado: Response<List<AlbumsDataResponse>> = retrofit.getAlbums()
-            withContext(Dispatchers.Main){
-                if (resultado.isSuccessful) {
-                    Log.i("ejemplo", "$resultado")
-                    Log.i("ejemplo", "${resultado.body()}")
-                }
-            }
-        }
+
         setContent {
             CorrutinasTheme {
                 Surface {
-                    SuperHeroList()
+                    var retrofit = RetrofitHelper.getInstance()
+                    val albums = remember { mutableStateOf<List<AlbumsDataResponse>>(emptyList())  }
+
+                    lifecycleScope.launch(Dispatchers.IO){
+                        var resultado = retrofit.getAlbums()
+                        withContext(Dispatchers.Main){
+                            if (resultado.isSuccessful) {
+                                albums.value = resultado.body() ?: emptyList()
+                            }
+                        }
+                    }
+                    SuperHeroList(albums = albums.value)
                 }
             }
         }
@@ -45,10 +54,18 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun SuperHeroList() {
-    Text(
-        text = "hoila"
-    )
+fun SuperHeroList(albums: List<AlbumsDataResponse>) {
+
+    LazyColumn (modifier = Modifier
+        .padding(10.dp)
+        .fillMaxSize()
+        ) {
+            items(albums) { album ->
+                Text(
+                    text = "${album.userID} / ${album.id} / ${album.title}",
+                )
+            }
+        }
 
 
 }
